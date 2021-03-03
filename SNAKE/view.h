0855a314484e7545aeaea07ad17a1a676f34f69f
Gileksa <70900137+Gileksa@users.noreply.github.com>
2021-03-03 23:17:36 +0300

    Update view.h

--- SNAKE/view.h
+++ SNAKE/view.h
@@ -1,9 +1,15 @@
+#pragma once
+
+#include "game.h"
+
 class View
 {
 	virtual void draw() = 0;
-	
+		
 	public:
-	~View();
-};
+	virtual ~View() {} //деструктор не может быть абстрактным
+	virtual struct winsize WhatSize() = 0;
+	virtual void DrawRabbit(class Rabbit rabbit) = 0;
 
+};
 
