015d0862cebe637ae63187d12f9905ffed593343
Gileksa <70900137+Gileksa@users.noreply.github.com>
2021-03-03 23:16:07 +0300

    Update tui.cc

--- SNAKE/tui.cc
+++ SNAKE/tui.cc
@@ -1,13 +1,29 @@
 #include "tui.h"
-#include <sys/ioctl.h>
-#include <unistd.h>
 
-struct winsize WhatSize()
+using namespace std;
+
+function<void(void)> Tui::onwinch;
+
+Tui :: Tui()
+{
+	setbuf(stdout, NULL);			//дебуферизовать для красоты
+	draw();					//нарисовать рамочку
+
+	onwinch = bind(&Tui::draw, this);
+	signal(SIGWINCH, &Tui::winch);		//для ресайза
+};
+
+struct winsize Tui::WhatSize()
 {
 	struct winsize w;
     	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 	return w;
 }
+
+void Tui::winch(int n)
+{
+	onwinch();
+};
 
 void ClearScreen()
 {
@@ -24,13 +40,14 @@ void DrawHorisontal(int y_start, int x_start, int length)
 	int i;
 
 	GoTo(y_start, x_start);
-	printf("\e[47m");
+	printf("\e[47m");		//яет цвет фона на белый
 	for (i = 0; i < length; i++)
 	{
 		printf(" ");
 		GoTo(y_start, x_start + i + 1);
 	}
-	printf(" \e[0m");				//вспомнить, как убрать эту некрасивость из кода
+	printf(" \e[0m");			//вспомнить, как убрать эту некрасивость из кода
+						//возвращает чёрный цвет фона	
 };
 
 void DrawVertical(int y_start, int x_start, int length)
@@ -47,45 +64,33 @@ void DrawVertical(int y_start, int x_start, int length)
 	printf("  \e[0m");
 }
 
-void DrawFlame(int lines, int columns)
+void DrawFrame(int lines, int columns)
 {
-////////с помощью прохода по экрану
-	/*int i = 0, j = 0;
-	for(i = 0; i < (columns - 1); i++)	
-		printf("*");
-
-	for(j = 0; j < (lines - 3); j++)
-	{
-		printf("**");
-		for(i = 1; i < (columns - 1); i++)
-			printf(" ");
-	}
-
-	for(i = 0; i < (columns + 1); i++)	
-		printf("*");*/
-
-////////интересней
 	DrawHorisontal(0, 0, columns);
 	DrawVertical(1, 0, lines - 2);
-	DrawVertical(1, columns, (lines - 2));
+	DrawVertical(1, columns - 1, (lines - 2));
 	DrawHorisontal(lines -1, 0, columns);
 };
 
-Tui :: Tui()
+void Tui::DrawRabbit(class Rabbit rabbit)
+{
+	//переместить курсор в координаты кролика
+	struct Coord c = rabbit.WhatCoords();
+	GoTo(c.y, c.x);
+	printf("\e[46m \e[0m");
+};
+
+
+
+void Tui :: draw()
 {
 	GoTo(0,0); 
 	ClearScreen(); 				//очистили экран, надо узнать его размеры
 	struct winsize w = WhatSize(); 		//w.ws_row - lines, w.ws_col - columns
- 	DrawFlame(w.ws_row,w.ws_col);
-	
+ 	DrawFrame(w.ws_row,w.ws_col);
 };
 
 Tui::~Tui()
 {
-
-};
-
-void Tui :: draw()
-{
-
+	
 };
