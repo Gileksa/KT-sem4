7ecdb59fd4bef50dc074d966d11a8ac80f2615df
Gileksa <70900137+Gileksa@users.noreply.github.com>
2021-03-03 23:15:06 +0300

    Update main.cc

--- SNAKE/main.cc
+++ SNAKE/main.cc
@@ -1,9 +1,33 @@
 #include "tui.h"
+#include "gui.h"
+
+#include <string.h>
+#include <iostream>
 
 int main(int ac, char** av)
 {
-	Tui *ryr = new Tui;
-	delete ryr;
+	
+	View *v;
+	
+	/*if(strcmp(av[1], "gui"))
+	{
+		v = new Gui;
+		
+	}
+	
+	else
+	{
+		v = new Tui;
+		
+	}*/
 
+	v = new Tui;
+	Game m(v);
+	list<Rabbit> rabbits = m.Rabbits();
+	v->DrawRabbit(rabbits.back());
+
+	getchar();
+
+	//delete v;
 	return 0;
 }
